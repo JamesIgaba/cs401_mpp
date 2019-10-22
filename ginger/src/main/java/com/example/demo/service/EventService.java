e8062197a07f404f9b1a8927ad917fd8b569c85c
james <james@james-Latitude-E7250>
2019-10-22 12:00:00 -0500

    Event service

---
+++ ginger/src/main/java/com/example/demo/service/EventService.java
@@ -0,0 +1,45 @@
+package com.example.demo.service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Event;
+import com.example.demo.repository.EventRepository;
+
+@Service
+public class EventService {
+	
+	@Autowired
+	private EventRepository eventRepo;
+	
+	//create event
+	public Event createEventService( String userEmailId, String name, String description, int day, int month, int year) {
+		return eventRepo.save(new Event(userEmailId, name,description, LocalDate.of(year,month,day)));
+	}
+	
+	//delete event
+	public void delete(String eventId) {
+		eventRepo.deleteById(eventId);
+	}
+	
+	//update event
+	public Event updateEvent(String eventId,String userEmailId, String name, String description, int day, int month, int year) {
+		Optional<Event> event = eventRepo.findById(eventId);
+		if(event.isPresent()) {
+			event.get().setName(name);
+			event.get().setDescription(description);
+			event.get().setDateOfEvent(LocalDate.of(year, month, day));
+		}
+		return eventRepo.save(event.get());
+	}
+	
+	//search events created by a user
+	public List<Event> searchEvents(String userEmailId){
+		return eventRepo.findByUserEmailId(userEmailId);
+	}
+
+}
